baa94d2db1c25b98b0f86a61c610f1de5b86abbf
Sandy Maguire <sandy@sandymaguire.me>
2019-02-17 11:09:55 -0500

    vextab and analytics

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@
 echo "deploying on server..."
 stack build
 stack exec poly
-rsync -r -a -v -e "ssh -i $HOME/.ssh/santino.pem" dist/ "ubuntu@$SERENADE:/data/blog/_live/reasonably-polymorphic/"
+rsync -r -a -v -e "ssh -i $HOME/.ssh/santino.pem" dist/ "ubuntu@$SERENADE:/data/blog/_live/egbdf/"
 
